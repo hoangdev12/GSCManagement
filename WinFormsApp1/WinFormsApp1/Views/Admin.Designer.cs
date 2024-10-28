6b33bb21768ffea4622eaa53843b1f59c239fedb
hoang12 <hoangluucoder1702@gmail.comgit config --global user.email hoangluucoder1702@gmail.com>
2024-10-28 09:13:50 +0700

    update

---
+++ WinFormsApp1/WinFormsApp1/Views/Admin.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WinFormsApp1.Views
+{
+    partial class Admin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            Viewtest = new Button();
+            dataAccount = new DataGridView();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataAccount).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(27, 15);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(743, 48);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(Viewtest);
+            panel2.Controls.Add(dataAccount);
+            panel2.Location = new Point(31, 83);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(739, 355);
+            panel2.TabIndex = 1;
+            // 
+            // Viewtest
+            // 
+            Viewtest.Location = new Point(231, 306);
+            Viewtest.Name = "Viewtest";
+            Viewtest.Size = new Size(94, 29);
+            Viewtest.TabIndex = 2;
+            Viewtest.Text = "button1";
+            Viewtest.UseVisualStyleBackColor = true;
+            Viewtest.Click += Viewtest_Click_1;
+            // 
+            // dataAccount
+            // 
+            dataAccount.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataAccount.Location = new Point(32, 25);
+            dataAccount.Name = "dataAccount";
+            dataAccount.RowHeadersWidth = 51;
+            dataAccount.Size = new Size(687, 261);
+            dataAccount.TabIndex = 1;
+            // 
+            // Admin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Admin";
+            Text = "AdminAccount";
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataAccount).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Button Viewtest;
+        private DataGridView dataAccount;
+    }
+}
---
+++ WinFormsApp1/WinFormsApp1/Views/Admin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using WinFormsApp1.Models;
+
+namespace WinFormsApp1.Views
+{
+    public partial class Admin : Form
+    {
+        private GcsmanagerContext _context;
+
+        public Admin()
+        {
+            InitializeComponent();
+            _context = new GcsmanagerContext(); // Khởi tạo DbContext
+            LoadAccountData(); // Tải dữ liệu khi form được mở
+        }
+
+        private void LoadAccountData()
+        {
+            try
+            {
+                // Lấy dữ liệu từ bảng Account
+                var accountData = _context.Accounts.ToList();
+
+                // Gán dữ liệu vào DataGridView
+                dataAccount.DataSource = accountData;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi tải dữ liệu: " + ex.Message);
+            }
+        }
+
+        private void Viewtest_Click(object sender, EventArgs e)
+        {
+            LoadAccountData();
+        }
+
+        private void Viewtest_Click_1(object sender, EventArgs e)
+        {
+            LoadAccountData();
+        }
+    }
+}
