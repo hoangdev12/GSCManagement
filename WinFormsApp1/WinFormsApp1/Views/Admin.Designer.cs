9abe579e037bd6542f4ad32d63b886f1b38e3f86
hoang12 <hoangluucoder1702@gmail.comgit config --global user.email hoangluucoder1702@gmail.com>
2024-10-31 20:56:48 +0700

    computer

--- WinFormsApp1/WinFormsApp1/Views/Admin.Designer.cs
+++ WinFormsApp1/WinFormsApp1/Views/Admin.Designer.cs
@@ -28,45 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            tabAdmin = new TabControl();
+            tabControl1 = new TabControl();
             tabPage1 = new TabPage();
             tabPage2 = new TabPage();
             tabPage3 = new TabPage();
             tabPage4 = new TabPage();
             tabPage5 = new TabPage();
+            dgvBooking = new DataGridView();
             tabPage6 = new TabPage();
             tabPage7 = new TabPage();
+            panel1 = new Panel();
+            panel4 = new Panel();
+            txtTongTien = new TextBox();
+            lblTongTien = new Label();
+            panel3 = new Panel();
+            txtConLai = new TextBox();
+            lblConLai = new Label();
+            panel2 = new Panel();
+            txtDaChoi = new TextBox();
+            lblDaChoi = new Label();
+            flpComputers = new FlowLayoutPanel();
             tabPage8 = new TabPage();
-            tabPage9 = new TabPage();
-            tabAdmin.SuspendLayout();
+            tabControl1.SuspendLayout();
+            tabPage5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvBooking).BeginInit();
+            tabPage7.SuspendLayout();
+            panel1.SuspendLayout();
+            panel4.SuspendLayout();
+            panel3.SuspendLayout();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
-            // tabAdmin
+            // tabControl1
             // 
-            tabAdmin.Controls.Add(tabPage1);
-            tabAdmin.Controls.Add(tabPage2);
-            tabAdmin.Controls.Add(tabPage3);
-            tabAdmin.Controls.Add(tabPage4);
-            tabAdmin.Controls.Add(tabPage5);
-            tabAdmin.Controls.Add(tabPage6);
-            tabAdmin.Controls.Add(tabPage7);
-            tabAdmin.Controls.Add(tabPage8);
-            tabAdmin.Controls.Add(tabPage9);
-            tabAdmin.Location = new Point(12, 12);
-            tabAdmin.Name = "tabAdmin";
-            tabAdmin.SelectedIndex = 0;
-            tabAdmin.Size = new Size(963, 460);
-            tabAdmin.TabIndex = 3;
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Controls.Add(tabPage4);
+            tabControl1.Controls.Add(tabPage5);
+            tabControl1.Controls.Add(tabPage6);
+            tabControl1.Controls.Add(tabPage7);
+            tabControl1.Controls.Add(tabPage8);
+            tabControl1.Location = new Point(12, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(967, 483);
+            tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
-            tabPage1.AutoScroll = true;
             tabPage1.Location = new Point(4, 29);
             tabPage1.Name = "tabPage1";
             tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(955, 427);
+            tabPage1.Size = new Size(959, 450);
             tabPage1.TabIndex = 0;
-            tabPage1.Text = "tabAdmin";
+            tabPage1.Text = "Account";
             tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
@@ -74,97 +90,219 @@
             tabPage2.Location = new Point(4, 29);
             tabPage2.Name = "tabPage2";
             tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(955, 427);
+            tabPage2.Size = new Size(959, 450);
             tabPage2.TabIndex = 1;
-            tabPage2.Text = "Account";
+            tabPage2.Text = "Product";
             tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabPage3
             // 
             tabPage3.Location = new Point(4, 29);
             tabPage3.Name = "tabPage3";
-            tabPage3.Size = new Size(955, 427);
+            tabPage3.Size = new Size(959, 450);
             tabPage3.TabIndex = 2;
-            tabPage3.Text = "Product";
+            tabPage3.Text = "Customer";
             tabPage3.UseVisualStyleBackColor = true;
             // 
             // tabPage4
             // 
             tabPage4.Location = new Point(4, 29);
             tabPage4.Name = "tabPage4";
-            tabPage4.Size = new Size(955, 427);
+            tabPage4.Size = new Size(959, 450);
             tabPage4.TabIndex = 3;
-            tabPage4.Text = "Customer";
+            tabPage4.Text = "Categories";
             tabPage4.UseVisualStyleBackColor = true;
             // 
             // tabPage5
             // 
+            tabPage5.Controls.Add(dgvBooking);
             tabPage5.Location = new Point(4, 29);
             tabPage5.Name = "tabPage5";
-            tabPage5.Size = new Size(955, 427);
+            tabPage5.Size = new Size(959, 450);
             tabPage5.TabIndex = 4;
-            tabPage5.Text = "Categories";
+            tabPage5.Text = "Booking";
             tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // dgvBooking
+            // 
+            dgvBooking.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvBooking.Location = new Point(10, 13);
+            dgvBooking.Name = "dgvBooking";
+            dgvBooking.RowHeadersWidth = 51;
+            dgvBooking.Size = new Size(946, 432);
+            dgvBooking.TabIndex = 0;
             // 
             // tabPage6
             // 
             tabPage6.Location = new Point(4, 29);
             tabPage6.Name = "tabPage6";
-            tabPage6.Size = new Size(955, 427);
+            tabPage6.Size = new Size(959, 450);
             tabPage6.TabIndex = 5;
-            tabPage6.Text = "Booking";
+            tabPage6.Text = "Payment";
             tabPage6.UseVisualStyleBackColor = true;
             // 
             // tabPage7
             // 
+            tabPage7.Controls.Add(panel1);
+            tabPage7.Controls.Add(flpComputers);
             tabPage7.Location = new Point(4, 29);
             tabPage7.Name = "tabPage7";
-            tabPage7.Size = new Size(955, 427);
+            tabPage7.Size = new Size(959, 450);
             tabPage7.TabIndex = 6;
-            tabPage7.Text = "Payment";
+            tabPage7.Text = "Computer";
             tabPage7.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(panel4);
+            panel1.Controls.Add(panel3);
+            panel1.Controls.Add(panel2);
+            panel1.Location = new Point(627, 8);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(324, 432);
+            panel1.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(txtTongTien);
+            panel4.Controls.Add(lblTongTien);
+            panel4.Location = new Point(3, 192);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(308, 59);
+            panel4.TabIndex = 3;
+            // 
+            // txtTongTien
+            // 
+            txtTongTien.Location = new Point(114, 19);
+            txtTongTien.Name = "txtTongTien";
+            txtTongTien.ReadOnly = true;
+            txtTongTien.Size = new Size(174, 27);
+            txtTongTien.TabIndex = 1;
+            // 
+            // lblTongTien
+            // 
+            lblTongTien.AutoSize = true;
+            lblTongTien.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblTongTien.Location = new Point(13, 18);
+            lblTongTien.Name = "lblTongTien";
+            lblTongTien.Size = new Size(95, 28);
+            lblTongTien.TabIndex = 0;
+            lblTongTien.Text = "Tổng tiền";
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(txtConLai);
+            panel3.Controls.Add(lblConLai);
+            panel3.Location = new Point(3, 108);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(308, 59);
+            panel3.TabIndex = 2;
+            // 
+            // txtConLai
+            // 
+            txtConLai.Location = new Point(114, 19);
+            txtConLai.Name = "txtConLai";
+            txtConLai.ReadOnly = true;
+            txtConLai.Size = new Size(174, 27);
+            txtConLai.TabIndex = 1;
+            // 
+            // lblConLai
+            // 
+            lblConLai.AutoSize = true;
+            lblConLai.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblConLai.Location = new Point(18, 16);
+            lblConLai.Name = "lblConLai";
+            lblConLai.Size = new Size(72, 28);
+            lblConLai.TabIndex = 0;
+            lblConLai.Text = "Còn lại";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(txtDaChoi);
+            panel2.Controls.Add(lblDaChoi);
+            panel2.Location = new Point(3, 33);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(308, 59);
+            panel2.TabIndex = 0;
+            // 
+            // txtDaChoi
+            // 
+            txtDaChoi.Location = new Point(114, 19);
+            txtDaChoi.Name = "txtDaChoi";
+            txtDaChoi.ReadOnly = true;
+            txtDaChoi.Size = new Size(174, 27);
+            txtDaChoi.TabIndex = 1;
+            // 
+            // lblDaChoi
+            // 
+            lblDaChoi.AutoSize = true;
+            lblDaChoi.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblDaChoi.Location = new Point(18, 16);
+            lblDaChoi.Name = "lblDaChoi";
+            lblDaChoi.Size = new Size(83, 28);
+            lblDaChoi.TabIndex = 0;
+            lblDaChoi.Text = "Đã chơi ";
+            // 
+            // flpComputers
+            // 
+            flpComputers.Location = new Point(6, 8);
+            flpComputers.Name = "flpComputers";
+            flpComputers.Size = new Size(615, 437);
+            flpComputers.TabIndex = 0;
             // 
             // tabPage8
             // 
             tabPage8.Location = new Point(4, 29);
             tabPage8.Name = "tabPage8";
-            tabPage8.Size = new Size(955, 427);
+            tabPage8.Size = new Size(959, 450);
             tabPage8.TabIndex = 7;
-            tabPage8.Text = "Computer";
+            tabPage8.Text = "Employee";
             tabPage8.UseVisualStyleBackColor = true;
-            // 
-            // tabPage9
-            // 
-            tabPage9.Location = new Point(4, 29);
-            tabPage9.Name = "tabPage9";
-            tabPage9.Size = new Size(955, 427);
-            tabPage9.TabIndex = 8;
-            tabPage9.Text = "Employee";
-            tabPage9.UseVisualStyleBackColor = true;
             // 
             // Admin
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(994, 495);
-            Controls.Add(tabAdmin);
+            ClientSize = new Size(979, 498);
+            Controls.Add(tabControl1);
             Name = "Admin";
-            Text = "AdminAccount";
-            tabAdmin.ResumeLayout(false);
+            Text = "Admin";
+            tabControl1.ResumeLayout(false);
+            tabPage5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dgvBooking).EndInit();
+            tabPage7.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private TabControl tabAdmin;
+        private TabControl tabControl1;
         private TabPage tabPage1;
         private TabPage tabPage2;
         private TabPage tabPage3;
         private TabPage tabPage4;
         private TabPage tabPage5;
+        private DataGridView dgvBooking;
         private TabPage tabPage6;
         private TabPage tabPage7;
         private TabPage tabPage8;
-        private TabPage tabPage9;
+        private Panel panel1;
+        private FlowLayoutPanel flpComputers;
+        private Panel panel2;
+        private Label lblDaChoi;
+        private Panel panel4;
+        private TextBox txtTongTien;
+        private Label lblTongTien;
+        private Panel panel3;
+        private TextBox txtConLai;
+        private Label lblConLai;
+        private TextBox txtDaChoi;
     }
 }
--- WinFormsApp1/WinFormsApp1/Views/Admin.cs
+++ WinFormsApp1/WinFormsApp1/Views/Admin.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data.Entity;
+using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
 using WinFormsApp1.Models;
@@ -7,39 +9,159 @@ namespace WinFormsApp1.Views
 {
     public partial class Admin : Form
     {
-        private GcsmanagerContext _context;
+        private readonly GcsmanagerContext _context;
+        private System.Windows.Forms.Timer timerUpdate; // Chỉ định rõ Timer từ System.Windows.Forms
+        private Computer selectedComputer; // Biến lưu trữ máy tính được chọn
+        private DateTime startTime; // Thời gian bắt đầu
+        private const double PricePerHour = 10000; // Số tiền mỗi giờ
 
         public Admin()
         {
             InitializeComponent();
-            _context = new GcsmanagerContext(); // Khởi tạo DbContext
-            LoadAccountData(); // Tải dữ liệu khi form được mở
+            _context = new GcsmanagerContext();
+            loadDataBooking();
+            LoadComputers();
+
+            // Cấu hình Timer
+            timerUpdate = new System.Windows.Forms.Timer(); // Chỉ định rõ
+            timerUpdate.Interval = 1000; // Cập nhật mỗi giây
+            timerUpdate.Tick += TimerUpdate_Tick; // Đăng ký sự kiện
         }
 
-        private void LoadAccountData()
+        // Cập nhật thời gian mỗi giây
+        private void TimerUpdate_Tick(object sender, EventArgs e)
         {
-            try
+            UpdateRemainingTime(); // Gọi phương thức cập nhật thời gian
+        }
+
+        // Tải dữ liệu booking
+        public void loadDataBooking()
+        {
+            var dataBooking = _context.Bookings.ToList();
+            dgvBooking.DataSource = dataBooking;
+        }
+
+        // Tải danh sách máy tính
+        private void LoadComputers()
+        {
+            // Xóa các control cũ nếu có
+            flpComputers.Controls.Clear();
+
+            // Truy vấn dữ liệu từ bảng Computers
+            var computers = _context.Computers.ToList();
+
+            foreach (var computer in computers)
             {
-                // Lấy dữ liệu từ bảng Account
-                var accountData = _context.Accounts.ToList();
+                Button btn = new Button();
+                btn.Width = 100;
+                btn.Height = 80;
+                btn.Text = computer.ComputerName;
+                btn.BackColor = computer.Status == "Available" ? Color.LightGreen : Color.LightCoral;
 
-                // Gán dữ liệu vào DataGridView
-                dataAccount.DataSource = accountData;
+                // Gán tag cho nút để lưu trữ thông tin về máy tính
+                btn.Tag = computer;
+
+                // Đăng ký sự kiện click cho nút
+                btn.Click += (sender, e) =>
+                {
+                    // Lưu thông tin máy tính được chọn
+                    selectedComputer = computer;
+
+                    // Lấy thông tin Booking cho máy tính được nhấn
+                    var booking = _context.Bookings.FirstOrDefault(b => b.ComputerId == computer.ComputerId);
+                    if (booking != null)
+                    {
+                        bindingComputer(computer, booking);
+                        startTime = DateTime.Now; // Lưu thời gian bắt đầu
+                        timerUpdate.Start(); // Bắt đầu Timer khi có booking
+                    }
+                    else
+                    {
+                        txtConLai.Text = "Không có booking nào cho máy tính này.";
+                        txtDaChoi.Text = "0:0:0"; // Đặt lại thời gian đã chơi nếu không có booking
+                        txtTongTien.Text = "0"; // Đặt lại tổng tiền
+                    }
+                };
+
+                flpComputers.Controls.Add(btn);
             }
-            catch (Exception ex)
+        }
+
+        // Ràng buộc dữ liệu
+        private void bindingComputer(Computer computer, Booking booking)
+        {
+            if (booking.StartTime.HasValue)
             {
-                MessageBox.Show("Lỗi khi tải dữ liệu: " + ex.Message);
+                // Lấy thông tin khách hàng
+                var customer = _context.Customers.FirstOrDefault(c => c.CustomerId == booking.CustomerId);
+                if (customer != null)
+                {
+                    // Chuyển đổi Balance sang double
+                    double customerBalance = customer.Balance.HasValue ? (double)customer.Balance.Value : 0;
+
+                    // Tính số giờ còn lại dựa vào số tiền còn lại
+                    double remainingHours = customerBalance / PricePerHour;
+
+                    // Chuyển đổi số giờ còn lại thành TimeSpan
+                    TimeSpan remainingTime = TimeSpan.FromHours(remainingHours);
+
+                    // Hiển thị thời gian còn lại theo định dạng h:m:s
+                    txtConLai.Text = $"{(int)remainingTime.TotalHours}:{remainingTime.Minutes}:{remainingTime.Seconds}";
+                    txtDaChoi.Text = "0:0:0"; // Khởi tạo thời gian đã chơi
+                    txtTongTien.Text = "0"; // Khởi tạo tổng tiền
+                }
+                else
+                {
+                    txtConLai.Text = "Khách hàng không tồn tại";
+                }
+            }
+            else
+            {
+                txtConLai.Text = "N/A"; // Hoặc bất kỳ thông báo nào phù hợp
             }
         }
 
-        private void Viewtest_Click(object sender, EventArgs e)
+        // Cập nhật thời gian còn lại và thời gian đã chơi
+        private void UpdateRemainingTime()
         {
-            LoadAccountData();
-        }
+            if (selectedComputer != null)
+            {
+                var booking = _context.Bookings.FirstOrDefault(b => b.ComputerId == selectedComputer.ComputerId);
+                if (booking != null && booking.StartTime.HasValue)
+                {
+                    DateTime now = DateTime.Now;
 
-        private void Viewtest_Click_1(object sender, EventArgs e)
-        {
-            LoadAccountData();
+                    // Tính toán thời gian đã chơi
+                    TimeSpan durationPlayed = now - startTime;
+
+                    // Tính số giờ còn lại dựa vào số tiền còn lại
+                    double customerBalance = (double)(_context.Customers.FirstOrDefault(c => c.CustomerId == booking.CustomerId)?.Balance ?? 0);
+                    double remainingHours = customerBalance / PricePerHour;
+
+                    // Chuyển đổi số giờ còn lại thành TimeSpan
+                    TimeSpan remainingTime = TimeSpan.FromHours(remainingHours);
+
+                    // Cập nhật thời gian đã chơi
+                    txtDaChoi.Text = $"{(int)durationPlayed.TotalHours}:{durationPlayed.Minutes}:{durationPlayed.Seconds}";
+
+                    // Tính tổng tiền dựa theo số giây đã chơi
+                    double moneyPerSecond = PricePerHour / 3600; // Tính tiền mỗi giây
+                    double totalMoney = Math.Floor(durationPlayed.TotalSeconds * moneyPerSecond); // Tính tổng tiền
+                    txtTongTien.Text = totalMoney.ToString(); // Hiển thị tổng tiền
+
+                    // Cập nhật thời gian còn lại
+                    TimeSpan updatedRemainingTime = remainingTime - durationPlayed;
+                    if (updatedRemainingTime.TotalSeconds > 0)
+                    {
+                        txtConLai.Text = $"{(int)updatedRemainingTime.TotalHours}:{updatedRemainingTime.Minutes}:{updatedRemainingTime.Seconds}";
+                    }
+                    else
+                    {
+                        txtConLai.Text = "Đã hết thời gian";
+                        timerUpdate.Stop(); // Dừng timer nếu thời gian đã hết
+                    }
+                }
+            }
         }
     }
 }
